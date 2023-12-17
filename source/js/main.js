ef4af966f635b311fa6534590ad4cf00b4aa7a4d
Nazariy <nazarpoptsov@gmail.com>
2023-12-17 18:47:01 +0200

    Grag Stop Was added

--- source/js/main.js
+++ source/js/main.js
@@ -12,7 +12,23 @@ $(document).ready(function(){
     });
 
     function onMouseMove(event) {
-        // console.log(event.clientY)
-        blockToMove.offset({top: event.pageY - 8})
+
+        let blockOffsetTop = blockToMove.offset().top;
+        let blockHeigh = parseInt(blockToMove.css("height"))
+        let windowHeight = $(window).height();
+        let windowSctollTop = $(window).scrollTop()
+        let blockOffsetTopToWindow = blockOffsetTop-blockHeigh;
+
+        blockToMove.offset({top: (event.clientY - 10) + windowSctollTop})
+
+
+        // console.clear()
+        console.log(event)
+        console.log(`clientY: ${event}\n  blockOffsetTop: ${blockOffsetTop}\n blockHeight: ${blockHeigh}\n windowHeight: ${windowHeight}\n windowSctollTop: ${windowSctollTop}\n blockOffsetTopToWindow: ${blockOffsetTopToWindow}`)
+
+
+        if(event.clientY + blockHeigh < windowHeight){
+            blockToMove.offset({top: (windowHeight - blockHeigh) + windowSctollTop})
+        }
     }
 })
