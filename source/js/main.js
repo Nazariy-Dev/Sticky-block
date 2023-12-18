1a8ac8ee58a67105d54f953af204f96b09b32b57
Nazariy <nazarpoptsov@gmail.com>
2023-12-18 11:18:02 +0200

    Block border animation added

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function () {
     let touchToMove = $(".sticky-block__drag-to-move")
     let blockToMove = $(".sticky-block")
 
-    touchToMove.mousedown(function(){
+
+    touchToMove.mousedown(function () {
+        blockToMove.css({
+            "transition": "top 0s, border-radius .3s"
+
+        })
+
         $(document).mousemove(onMouseMove);
         console.log("mousedown")
     });
-    $(document).mouseup(function(){
+
+    $(document).mouseup(function (event) {
         $(document).off("mousemove", onMouseMove)
-        console.log("mouseup")
+
+        let blockOffsetTop = blockToMove.offset().top;
+        let blockHeigh = parseInt(blockToMove.css("height"))
+        let windowHeight = $(window).height();
+        let windowSctollTop = $(window).scrollTop()
+        let blockOffsetTopToWindow = blockOffsetTop - windowSctollTop;
+
+        if (blockOffsetTopToWindow + blockHeigh > windowHeight) {
+
+            blockToMove.css({
+                "top": "75%",
+                "transition": "top .3s, border-radius .3s",
+            })
+        }
     });
 
     function onMouseMove(event) {
@@ -17,18 +37,21 @@ $(document).ready(function(){
         let blockHeigh = parseInt(blockToMove.css("height"))
         let windowHeight = $(window).height();
         let windowSctollTop = $(window).scrollTop()
-        let blockOffsetTopToWindow = blockOffsetTop-blockHeigh;
+        let blockOffsetTopToWindow = event.clientY;
 
-        blockToMove.offset({top: (event.clientY - 10) + windowSctollTop})
+        blockToMove.offset({ top: (event.clientY - 10) + windowSctollTop })
 
-
-        // console.clear()
-        console.log(event)
         console.log(`clientY: ${event}\n  blockOffsetTop: ${blockOffsetTop}\n blockHeight: ${blockHeigh}\n windowHeight: ${windowHeight}\n windowSctollTop: ${windowSctollTop}\n blockOffsetTopToWindow: ${blockOffsetTopToWindow}`)
 
-
-        if(event.clientY + blockHeigh < windowHeight){
-            blockToMove.offset({top: (windowHeight - blockHeigh) + windowSctollTop})
+        if (blockOffsetTopToWindow + blockHeigh < windowHeight) {
+            blockToMove.offset({ top: (windowHeight - blockHeigh) + windowSctollTop })
+            blockToMove.css({
+                "border-radius": "0"
+            })
+        } else {
+            blockToMove.css({
+                "border-radius": "20px 20px 0 0"
+            })
         }
     }
 })
